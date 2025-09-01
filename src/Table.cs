d28275778d2cacd3bb4127c50f4b34f3232f2a71
Patricio <PATRICIO.BARRON@correo.ucu.edu.uy>
2025-09-01 10:37:29 -0300

    Termine la Table

--- src/Table.cs
+++ src/Table.cs
@@ -8,10 +8,46 @@ namespace Ucu.Poo.Restaurant
     public class Table
     {
         private List<Dish> order = new List<Dish>();
+        private int Number {get; set;}
+        private bool IsOccupied {get; set;}
 
+        public Table(int num)
+        {
+            Number = num;
+            IsOccupied = false;
+        }
+
+        public void AddToOrder(Dish dish)
+        {
+            order.Add(dish);
+        }
+        
+        
+        public void Occupy()
+        {
+            IsOccupied = true;
+        }
+        
+        public void Free()
+        {
+            IsOccupied = false;
+            order.Clear();
+        }
+        
         public bool HasOrders()
         {
-            return this.order.Count > 0;
-        }
+            bool resultado;
+            if (order.Count == 0)
+            {
+                resultado = false;
+                
+                
+            }
+            else
+            {
+                resultado = true;
+            }
+            return resultado;
+        }   
     }
 }
